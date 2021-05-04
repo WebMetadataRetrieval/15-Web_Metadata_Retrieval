00eeec9abab2b907bde11e1713f90a6986c637f5
Deven Vachhani <50715966+devsoftzz@users.noreply.github.com>
2021-05-04 20:23:03 +0530

    API Documentation Link Added

--- Code/venv/src/Content/views.py
+++ Code/venv/src/Content/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 def home_view(request):
     return render(request, "home.html")
 
-
-# TODO Build This Page
 def doc_view(request):
-    return render(request, "home.html")
+    return redirect("https://docs.google.com/document/d/1chmmfWFlP5Hf5uhSkZ_u31DODUu3Trtp4VTlmLdkvDQ/edit")
