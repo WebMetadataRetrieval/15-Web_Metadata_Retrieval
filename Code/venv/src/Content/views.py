0316ce26d19708ad55cae267a5d4754920f135f8
Manan Joshi <mananj0806@gmail.com>
2021-05-07 00:34:38 +0530

    Worked on issue #63

--- Code/venv/src/Content/views.py
+++ Code/venv/src/Content/views.py
@@ -6,4 +6,5 @@ def home_view(request):
     return render(request, "home.html")
 
 def doc_view(request):
-    return redirect("https://docs.google.com/document/d/1chmmfWFlP5Hf5uhSkZ_u31DODUu3Trtp4VTlmLdkvDQ/edit")
+    # return redirect("https://docs.google.com/document/d/1chmmfWFlP5Hf5uhSkZ_u31DODUu3Trtp4VTlmLdkvDQ/edit")
+    return render(request, "documentation.html")
